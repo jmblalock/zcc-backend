5bab8bb28885a120556443a574b084214f129409
Justin Blalock <justin.m.blalock@gmail.com>
2021-08-06 11:29:18 -0500

    first commit

---
+++ keystone.ts
@@ -0,0 +1,84 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { Role } from './schemas/Role';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { User } from './schemas/User';
+import { CartItem } from './schemas/CartItem';
+import 'dotenv/config';
+import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
+import { permissionsList } from './schemas/fields';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('Connected to the database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+      CartItem,
+      OrderItem,
+      Order,
+      Role,
+    }),
+    extendGraphqlSchema,
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: `id name email role { ${permissionsList.join(' ')} }`,
+    }),
+  })
+);
---
+++ mutations/addToCart.ts
@@ -0,0 +1,36 @@
+import { KeystoneContext } from '@keystone-next/types';
+import { CartItemCreateInput } from '../.keystone/schema-types';
+import { Session } from '../types';
+
+export default async function addToCart(
+  root: any,
+  { productId }: { productId: string },
+  context: KeystoneContext
+): Promise<CartItemCreateInput> {
+  // 1. Query the current user, see if they are signed in
+  const sesh = context.session as Session;
+  if (!sesh.itemId) {
+    throw new Error('You must be logged in to do this!');
+  }
+  // 2. Query the current users cart
+  const allCartItems = await context.lists.CartItem.findMany({
+    where: { user: { id: sesh.itemId }, product: { id: productId } },
+    resolveFields: 'id,quantity',
+  });
+  const [existingCartItem] = allCartItems;
+  if (existingCartItem) {
+    // 3. See if the current item is in the cart
+    // a. if it is, increment by 1
+    return await context.lists.CartItem.updateOne({
+      id: existingCartItem.id,
+      data: { quantity: existingCartItem.quantity + 1 },
+    });
+  }
+  // b. if it isn't, create a new cart item!
+  return await context.lists.CartItem.createOne({
+    data: {
+      product: { connect: { id: productId } },
+      user: { connect: { id: sesh.itemId } },
+    },
+  });
+}
