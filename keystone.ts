3765007d99ef9edce818ac143126b395f9dc37f9
Justin Blalock <justin.m.blalock@gmail.com>
2021-08-10 17:16:39 -0500

    added cors methods to withAuth config keystone.ts

--- keystone.ts
+++ keystone.ts
@@ -57,6 +57,7 @@ export default withAuth(
       cors: {
         origin: [process.env.FRONTEND_URL],
         credentials: true,
+        methods: process.env.CORS_METHODS,
       },
     },
     db: {
